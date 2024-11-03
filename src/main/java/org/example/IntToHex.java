33aa5e627a38bf5f86bc63f1589b432d0b92fab4
cem1n <cemenpervakov@gmail.com>
2024-11-03 12:26:03 +0300

    d

--- src/main/java/org/example/IntToHex.java
+++ src/main/java/org/example/IntToHex.java
@@ -1,7 +1,36 @@
 package org.example;
 
 public class IntToHex {
-    public String intToHex(int value){
-        return" ";
+    public static int searchForRemainder (int x){
+        int remainder = 0;
+        for(int index = 0;index < 4;++index){
+            remainder = remainder + (x & 1) * (int)(Math.pow(2, index));
+            x >>= 1;
+        }
+        return remainder;
+    }
+
+    public static String intToHex(int value) {
+        String[] mas = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+        StringBuilder Hex = new StringBuilder();
+        boolean negative = value < 0;
+
+        if (value == 0) {
+            return "0";
+        }
+
+        if (negative) {
+            value = value * -1;
+        }
+
+        while (value != 0) {
+            int c = searchForRemainder(value);
+            Hex.insert(0, mas[c]);
+            value >>= 4;
+        }
+        if (negative) {
+            Hex.insert(0, "-");
+        }
+        return Hex.toString();
     }
 }
--- src/test/java/org/example/IntToHexTest.java
+++ src/test/java/org/example/IntToHexTest.java
@@ -1,4 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.example.IntToHex.intToHex;
+import static org.junit.Assert.assertEquals;
+
 class IntToHexTest {
-  
+    @Test
+    void test1() {
+        for (int index = -10000; index < 10000; index++) {
+            String conv = Integer.toString(index, 16);
+            System.out.println(index + " - value");
+            System.out.println(conv + " - conv");
+            System.out.println();
+            assertEquals(conv, intToHex(index));
+        }
+    }
+
+    @Test
+    void test2() {
+        assertEquals(Integer.toString(0, 16), intToHex(0));
+        assertEquals(Integer.toString(10000, 16), intToHex(10000));
+        assertEquals(Integer.toString(-10000, 16), intToHex(-10000));
+    }
+
+
+    @Test
+    void test3() {
+        assertEquals(Integer.toString(2147483647, 16), intToHex(2147483647));
+        assertEquals(Integer.toString(-2147483647, 16), intToHex(-2147483647));
+    }
 }
